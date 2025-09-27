7d03368bfed8c01bfec2fce687e0d224e3c0b3cb
Tauã Bernardo <tbptffc@gmail.com>
2025-09-27 15:01:14 -0300

    project creation

---
+++ src/main/java/com/example/wrong/WrongQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.wrong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrongQuestionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrongQuestionsApplication.class, args);
+	}
+
+}
